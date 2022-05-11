dc9516924ceb0d540e8cc39bf63f49b1197d7d8d
welison-coder <wellisson.marques@tc.df.gov.br>
2022-05-11 18:18:39 -0300

    atualização de dados na agenda

--- core/views.py
+++ core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login , logout
+from django.contrib import messages
 # Create your views here.
 
 def login_user(request):
@@ -19,8 +20,9 @@ def submit_login(request):
         if usuario is not None:
             login(request,usuario)
             return redirect('/')
-    else:
-        redirect('/')
+        else:
+            messages.error(request, "Usuario ou senha inválido!")
+    return redirect('/')
 
 
 @login_required(login_url='/login/')
@@ -32,6 +34,37 @@ def lista_eventos(request):
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = { }
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
-@login_required(login_url='/login/')..........
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        id_evento = request.POST.get('id_evento')
+        if id_evento:
+            Evento.objects.filter(id=id_evento).update(titulo=titulo,
+                                                       data_evento=data_evento,
+                                                       descricao=descricao)
+        else:
+
+            Evento.objects.create(titulo=titulo,
+                                  data_evento=data_evento,
+                                  descricao=descricao,
+                                  usuario=usuario)
+    return redirect('/')
+
+@login_required(login_url='/login/')
+def delete_evento(request,id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+       evento.delete()
+    return redirect('/')
+
